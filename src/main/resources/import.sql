fd13ed3f52144077b74d88eb8ce20eb7e35e9f9f
ivanrlmx99 <ivanlaureano77@gmail.com>
2020-06-21 00:36:20 -0500

    proyecto crud bueno

---
+++ src/main/resources/import.sql
@@ -0,0 +1,12 @@
+/* Populate tables */
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(88, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28');
+INSERT INTO clientes (id, nombre, apellido, email, create_at) VALUES(99, 'John', 'Doe', 'john.doe@gmail.com', '2017-08-28');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(1, 'Andres', 'Guzman', 'profesor@bolsadeideas.com', '2017-08-28');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(2, 'Ivan', 'Ramos', 'LicIformatica@bolsadeideas.com', '2020-08-28');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(3, 'Miguel', 'RamosG', 'LicIformatica@bolsadeideas.com', '2020-08-28');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(4, 'kevin', 'Ramos', 'LicIformatica@bolsadeideas.com', '2020-08-28');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(5, 'Neftali', 'Ramos', 'LicIformatica@bolsadeideas.com', '2020-08-28');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(6, 'Miguel', 'Ramos', 'LicIformatica@bolsadeideas.com', '2020-08-28');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(7, 'perla', 'Ramos', 'LicIformatica@bolsadeideas.com', '2020-08-28');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(9, 'kevin', 'Ramos', 'LicIformatica@bolsadeideas.com', '2020-08-28');
+INSERT INTO clientes(id, nombre, apellido, email, create_at) VALUES(10, 'Miguel Angel', 'Ramos', 'LicIformatica@bolsadeideas.com', '2020-08-28');
